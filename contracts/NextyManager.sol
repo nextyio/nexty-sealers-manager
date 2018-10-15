71e5f063b70d75814eb0fd53daa6138d7f8e61d8
bestboyvn87 <39051668+bestboyvn87@users.noreply.github.com>
2018-10-15 15:00:22 +0700

    NextyManager smart contract version 1.0

--- contracts/NextyManager.sol
+++ contracts/NextyManager.sol
@@ -1,101 +1,170 @@
+//solium-disable linebreak-style
 pragma solidity ^0.4.24;
 
 import "zos-lib/contracts/migrations/Migratable.sol";
-import "../access/Blacklist.sol";
+import "Blacklist.sol";
 
+contract NTFToken {
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+}
 
 /**
- * @title Nexty sealer management smart contract
+ * @title Nexty sealers management smart contract
  */
 contract NextyManager is Migratable, Blacklist {
-  mapping(address => uint256) balances;
-  mapping(address => address) public coinbase;
-  mapping(address => bool) public sealer;
-  
-  address[] public signers;
-  address[] public holders;
 
-  event SetCoinbase(address _holder, address _coinbase);
-  event UnSetCoinbase(address _holder, address _coinbase);
+    //minimum of deposited NTF to join
+    uint256 public constant MIN_NTF_AMOUNT = 100000;
+    //minimum blocks number distance from last leaved to current chain blocknumber to withdrawable
+    uint256 public constant MIN_BLOCKS_DISTANCE = 100;
 
-  /**
-   * Check if address is a valid destination to transfer tokens to
-   * - must not be zero address
-   * - must not be the token address
-   * - must not be the owner's address
-   * - must not be the sender's address
-   */
-  modifier validDestination(address to) {
-    require(to != address(0x0));
-    require(to != address(this));
-    require(to != owner);
-    require(to != msg.sender);
-    _;
-  }
+    enum SealerStatus {
+        PENDING_ACTIVE,     //Sealer deposited enough NTFs into registration contract successfully.
 
-  /**
-   * Token contract initialize
-   */
-  function initialize() isInitializer("Nexty", "0.1") public {
-  }
+        ACTIVE,             //Sealer send request to become a sealer 
+                            //and added into activation sealer set successfully
 
-  /**
-   * @dev Get all the list of current token holder.
-   * @return array of all token holder.
-   */
-  function getHolders() public view returns (address[]) {
-    return holders;
-  }
+        PENDING_WITHDRAW,   //Sealer send request to exit from activation sealer set successfully. 
+                            //Sealer casted out of activation sealer set
 
-  /**
-   * @dev Get all the list of current singer.
-   */
-  function getSigners() public view returns (address[]) {
-    return signers;
-  }
+        WITHDRAWN,          //Sealer already withdrawn their deposit NTFs successfully. 
+                            //They can only make withdrawal after withdrawal period.
 
-  /**
-   * Token holder can call method to set their coinbase for mining.
-   *
-   * @param _coinbase Destination address
-   */
-  function setCoinbase(address _coinbase) public validDestination(_coinbase) returns (bool) {
-    require(balances[msg.sender] > 0);
-    require(sealer[msg.sender] == false);
-    
-    coinbase[_coinbase] = msg.sender;
-    signers.push(_coinbase);
-    sealer[msg.sender] = true;
-    emit SetCoinbase(msg.sender, _coinbase);
-    return true;
-  }
-
-  /**
-   * Token holder can call method to remove their coinbase and reset to other coinbase later for mining.
-   *
-   * @param _coinbase Destination address
-   */
-  function unSetCoinbase(address _coinbase) public returns (bool) {
-    require(coinbase[_coinbase] == msg.sender);
-    require(sealer[msg.sender] == true);
-
-    delete coinbase[_coinbase];
-    removeSigner(_coinbase);
-    delete sealer[msg.sender];
-    emit UnSetCoinbase(msg.sender, _coinbase);
-    return true;
-  }
-
-  /**
-   * @dev Remove a specific address/account out of signer list
-   */
-  function removeSigner(address _signer) internal {
-    for (uint i = 0; i < signers.length; i++) {
-      if (_signer == signers[i]) {
-        signers[i] = signers[signers.length - 1];
-        signers.length--;
-        return;
-      }
+        PENALIZED           //Sealer marked as penalized node (update by consensus or voting result via dapp) 
+                            //and cannot become active sealer and cannot withdraw balance neither.
     }
-  }
+
+    struct Record {
+        SealerStatus status;
+        uint256 balance;
+        address coinbase;
+        uint256 leavedBlocknumber;
+    }
+
+    address public ntfAddress;
+
+    //get invester address of a coinbase
+    mapping(address => Record) public sealers;
+
+    //address[] public coinbases;
+    address[] public coinbases;
+    //check if address of a coinbase
+    mapping(address => bool) public isCoinbase;
+    mapping(address => address) public getSealer;
+
+    event Deposited(address _sealer, uint _tokens);
+    event Joined(address _sealer, address _coinbase);
+    event Leaved(address _sealer, address _coinbase);
+    event Withdrawn(address _sealer, uint256 _tokens);
+
+    /**
+    * Check if address is a valid destination to transfer tokens to
+    * - must not be zero address
+    * - must not be the token address
+    * - must not be the owner's address
+    * - must not be the sender's address
+    */
+    modifier validCoinbase(address _coinbase) {
+        require(!isCoinbase[_coinbase], "coinbase already used");
+        require(_coinbase != address(0x0), "coinbase zero");
+        require(_coinbase != address(this), "same contract's address");
+        require(_coinbase != owner, "same owner's address");
+        require(_coinbase != msg.sender, "same sender's address");
+        _;
+    }
+
+    modifier joinable() {
+        require(sealers[msg.sender][status] != SealerStatus.ACTIVE, "already joined ");
+        require(sealers[msg.sender][balance] >= MIN_NTF_AMOUNT, "not enough ntf");
+        _;
+    }
+
+    modifier leaveable() {
+        require(sealers[msg.sender][status] == SealerStatus.ACTIVE, "not joined ");
+        _;
+    }
+
+    modifier withdrawable() {
+        require(sealers[msg.sender][status] == SealerStatus.PENDING_WITHDRAW, "not pending for withdraw");
+        uint256 distance = block.number.sub(sealers[msg.sender][leavedBlocknumber]);
+        require(distance >= MIN_BLOCKS_DISTANCE, "still locked after leaving");
+        _;
+    }
+
+    /**
+    * Token contract initialize
+    */
+    function initialize(address _ntfAddress) public isInitializer("NextyManager", "0.1") {
+        ntfAddress = _ntfAddress;
+    }
+
+    function addCoinbase(address _coinbase) internal {
+        isCoinbase[_coinbase] = true;
+        coinbases.push(_coinbase);
+    }
+
+    function removeCoinbase(address _coinbase) internal {
+        isCoinbase[_coinbase] = false;
+        for (uint i = 0; i < coinbases.length; i++) {
+            if (_coinbase == coinbases[i]) {
+                coinbases[i] = coinbases[coinbases.length - 1];
+                coinbases.length--;
+                return;
+            }
+        }
+    }
+
+    /**
+    * Transfer the NTF from token holder to registration contract. 
+    * Sealer might have to approve contract to transfer an amount of NTF before calling this function.
+    * @param _coinbase Destination address
+    */
+    function deposit(uint256 tokens) public returns (bool) {
+        NTFToken(ntfAddress).transferFrom(msg.sender, address(this), tokens);
+        sealers[msg.sender][balance] = sealers[msg.sender][balance].add(tokens);
+        emit Deposited(msg.sender, tokens);
+        return true;
+    }
+        
+    /**
+    * To allow deposited NTF participate joining in as sealer. 
+    * Participate already must deposit enough NTF via Deposit function. 
+    * It takes coinbase as parameter.
+    * @param _coinbase Destination address
+    */
+    function join(address _coinbase) public joinable validCoinbase(_coinbase) returns (bool) {
+        sealers[msg.sender][coinbase] = _coinbase;
+        sealers[msg.sender][status] = SealerStatus.ACTIVE;
+        getSealer[_coinbase] = msg.sender;
+        addCoinbase(_coinbase);
+        emit Joined(msg.sender, _coinbase);
+        return true;
+    }
+
+    /**
+    * Request to exit out of activation sealer set
+    */
+    function leave() public leaveable returns (bool) {
+        address _coinbase = sealers[msg.sender][coinbase];
+
+        sealers[msg.sender][coinbase] = 0x0;
+        sealers[msg.sender][status] = SealerStatus.PENDING_WITHDRAW;
+        sealers[msg.sender][leavedBlocknumber] = block.number;
+        delete getSealer[_coinbase];
+        removeCoinbase(_coinbase);
+        emit Leaved(msg.sender, _coinbase);
+        return true;
+    }
+
+    /**
+    * To withdraw sealer’s NTF balance when they already exited and after withdrawal period.
+    */
+    function withdraw() public withdrawable returns (bool) {
+        uint256 tokens = sealers[msg.sender][balance];
+        NTFToken(ntfAddress).transfer(msg.sender, tokens);
+        sealers[msg.sender][status] = SealerStatus.WITHDRAWN;
+        emit Withdrawn(msg.sender, tokens);
+        return true;
+    }
+
 }
